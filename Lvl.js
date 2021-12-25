ced12cbde8d603566b10c31ef1beba3daac63d9d
Gin-is-not-real <ninapariat@live.fr>
2021-12-25 23:08:49 +0100

    Update: Lvl bricks array is now multi array

--- Lvl.js
+++ Lvl.js
@@ -6,6 +6,7 @@ let lvl1 = [
 ];
 let lvl2 = [
     [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
     [null, null, 'o', 'o', 'r', 'r', 'o', 'o', null, null],
     [null, null, 'j', 'j', 'o', 'o', 'j', 'j', null, null],
     [null, null, 'j', 'j', 'j', 'j', 'j', 'j', null, null],
@@ -17,6 +18,8 @@ class Lvl {
     init(lvl) {
         let bricks = [];
         for(let i = 0; i < lvl.length; i++) {
+            let line = [];
+
             for(let j = 0; j < lvl[i].length; j++) {
                 let box = lvl[i][j];
                 let brick = null;
@@ -37,29 +40,39 @@ class Lvl {
                     let y = (i*brick.height)+1;
                     brick.init(x, y);
                 }
-                bricks.push(brick);
+                line.push(brick);
+                // bricks.push(brick);
             }
+            bricks.push(line);
         }
         this.bricks = bricks;
         console.log(bricks);
     }
 
     draw() {
-        this.bricks.forEach(brick => {
-            if(brick !== null) {
-                brick.draw();
-            }
-        });
+        this.bricks.forEach(line => {
+            line.forEach(brick => {
+                if(brick !== null) {
+                    brick.draw();
+                }
+            })
+        })
     }
 
     detectColisions() {
-        this.bricks.forEach(brick => {
-            if(brick !== null) {
-                let isTouched = brick.detectColision();
-                if(isTouched) {
-                    this.bricks[this.bricks.indexOf(brick)] = null;
+        let bricks = this.bricks;
+
+        for(let i = 0; i < bricks.length; i++) {
+            for(let j = 0; j < bricks[i].length; j++) {
+                let brick = bricks[i][j];
+                if(brick !== null) {
+                    let isTouched = brick.detectColision();
+                    if(isTouched) {
+                        bricks[i][j] = null;
+                    }
                 }
             }
-        })
+        }
+
     }
 }
