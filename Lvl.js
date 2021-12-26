d2a9d8179224e824d6e408b1d718dba06851e2c8
Gin-is-not-real <ninapariat@live.fr>
2021-12-26 18:08:26 +0100

    File Structure: move lvls array on a separate file

--- Lvl.js
+++ Lvl.js
@@ -1,30 +1,3 @@
-let lvl1 = [
-    ['j', 'o', 'o', 'o', 'r', 'r', 'o', 'o', 'o', 'j'],
-    ['j', 'o', 'o', 'o', 'r', 'r', 'o', 'o', 'o', 'j'],
-    ['j', 'j', 'j', 'o', 'o', 'o', 'o', 'j', 'j', 'j'],
-    ['j', 'j', 'j', 'j', 'o', 'o', 'j', 'j', 'j', 'j'],
-    ['j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', 'j'],
-    ['j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', 'j'],
-];
-let lvl2 = [
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null],
-    [null, null, 'o', 'o', 'r', 'r', 'o', 'o', null, null],
-    [null, null, 'j', 'j', 'o', 'o', 'j', 'j', null, null],
-    [null, null, 'j', 'j', 'j', 'j', 'j', 'j', null, null],
-];
-let lvlTest = [
-    ['j', 'o', 'o', 'j', 'r', 'r', 'j', 'o', 'o', 'j'],
-    ['j', 'o', 'o', 'j', 'r', 'r', 'j', 'o', 'o', 'j'],
-    ['j', 'o', 'o', 'j', 'r', 'r', 'j', 'o', 'o', 'j'],
-    ['j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', 'j'],
-    ['j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', 'j'],
-    ['j', 'o', 'o', 'j', 'r', 'r', 'j', 'o', 'o', 'j'],
-    ['j', 'o', 'o', 'j', 'r', 'r', 'j', 'o', 'o', 'j'],
-    ['j', 'o', 'o', 'j', 'r', 'r', 'j', 'o', 'o', 'j'],
-];
-let lvls = [lvl1, lvl2, lvlTest];
-
 class Lvl {
     bricks;
     exposed;
