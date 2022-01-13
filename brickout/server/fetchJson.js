6f9df6bef1eba7662389e07f87ae20b25dce6769
Gin-is-not-real <ninapariat@live.fr>
2022-01-13 02:00:31 +0100

    InProgress:

--- brickout/server/fetchJson.js
+++ brickout/server/fetchJson.js
@@ -7,6 +7,27 @@ function fetchScores() {
 }
 fetchScores();
 
+/**
+ * Get score
+ * @param {String} name 
+ */
+function findScore(name) {
+    let data = {
+        action: "find",
+        name: name,
+    };
+
+    let url = new URL(FOLDER_URL + '/scores.php');
+    for (let k in data) {
+        url.searchParams.append(k, data[k]);
+    }
+
+    fetch(url)
+    .then(response => response.text())
+    .then(response => console.log(response))
+}
+findScore('gin');
+
 function postScore(name, points) {
     let data = {
         name: name,
@@ -16,14 +37,13 @@ function postScore(name, points) {
     let url = new URL(FOLDER_URL + '/scores.php');
     for (let k in data) {
         url.searchParams.append(k, data[k]);
-      }
-    console.log(url)
+    }
 
     fetch(url)
     .then(response => response.text())
     .then(response => console.log(response))
 }
-postScore("gin", 200);
+postScore("gintest", 200);
 /**
  * test fetch put based on https://www.bezkoder.com/javascript-fetch/ tuto
  */
--- brickout/server/scores.php
+++ brickout/server/scores.php
@@ -1,13 +1,40 @@
 <?php
 $content = file_get_contents('scores.json');
 
-$to_put = '{"name": "' . $_GET['name'] . '", "points": ' . $_GET['points'] . '}';
 
-if(!empty($content)) {
-    $new_content = str_replace(']', ', ' . $to_put . ']', $content);
-}
-else {
-    $new_content = '[' . $to_put . ']';
+if(isset($_GET['action'])) {
+    $action = $_GET['action'];
+
+    if($action === 'find' AND !empty($content)) {
+        if(strpos('"' . $content, $_GET['name']) . '"') {
+            //index du premier char du name
+            $start_index = strpos($content, $_GET['name']);
+            $json_str = substr($content, $start_index -10);
+            $end_index = strpos($json_str, '},');
+
+            $json_str = substr($json_str, 0, ($end_index - $start_index));
+
+            echo $json_str;
+        }
+        else {
+        }
+    }
+    else if($action === 'post') {
+        $content = str_replace('[', '', $content);
+        $content = str_replace(']', '', $content);
+        $obj = json_decode($content);
+        
+        $to_put = '{"name": "' . $_GET['name'] . '", "points": ' . $_GET['points'] . '}';
+        
+        if(!empty($content)) {
+            // $new_content = str_replace(']', ', ' . $to_put . ']', $content);
+            $new_content = '[' . $content . ', ' . $to_put . ']';
+            file_put_contents('scores.json', $new_content, LOCK_EX);
+        }
+        else {
+            $new_content = '[' . $to_put . ']';
+            file_put_contents('scores.json', $new_content, LOCK_EX);
+        }
+    }
 }
 
-file_put_contents('scores.json', $new_content, LOCK_EX);
